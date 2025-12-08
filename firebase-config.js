c4a640f0339a310270beb468a0c71a0fe585ad59
sunnnnn <putawan.sra@gmail.com>
2025-12-08 23:14:45 +0700

    deployment

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: window.env.API_KEY,
+  authDomain: window.env.AUTH_DOMAIN,
+  databaseURL: window.env.DB_URL,
+  projectId: window.env.PROJECT_ID,
+  storageBucket: window.env.STORAGE_BUCKET,
+  messagingSenderId: window.env.MSG_SENDER_ID,
+  appId: window.env.APP_ID,
+};
--- firebase-config.template.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "__API_KEY__",
-  authDomain: "__AUTH_DOMAIN__",
-  databaseURL: "__DB_URL__",
-  projectId: "__PROJECT_ID__",
-  storageBucket: "__STORAGE_BUCKET__",
-  messagingSenderId: "__MSG_SENDER_ID__",
-  appId: "__APP_ID__"
-};
